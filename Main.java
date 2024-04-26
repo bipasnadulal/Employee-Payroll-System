1aa7c933fd99a2a9f2e082c2f4dd80338efe0c5a
Bipasna Dulal <bipasna.dulal75@gmail.com>
2024-04-26 07:53:54 +0545

    Added fulltime partime employee class in main.java

--- Main.java
+++ Main.java
@@ -16,4 +16,43 @@ abstract class Employee{
     public int getId(){
         return id;
     }
+
+    public abstract double calculateSalary();
+
+    @Override
+    public String toString(){
+        return "Employee[name="+name+", id="+id+",salary="+calculateSalary()+"]";
+    }
+}
+
+class FullTimeEmployee extends Employee{
+    private double monthlySalary;
+    public FullTimeEmployee(String name, int id, double monthlySalary){
+        super(name , id);
+        this.monthlySalary=monthlySalary;
+    }
+
+    @Override
+    public double calculateSalary(){
+        return monthlySalary;
+    }
+}
+
+class PartTimeEmployee extends Employee{
+    private int hoursWorked;
+    private double hourlyRate;
+    public PartTimeEmployee(String name, int id, int hoursWorked, double hourlyRate){
+        super(name, id);
+        this.hoursWorked=hoursWorked;
+        this.hourlyRate=hourlyRate;
+    }
+
+    @Override
+    public double calculateSalary(){
+        return hoursWorked*hourlyRate;
+    }
+}
+
+class PayrollSystem{
+    
 }
